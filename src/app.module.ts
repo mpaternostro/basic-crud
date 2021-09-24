50816f7259e4b28e401969c8ad8e6e57d640e454
Marcelo <marcelodpaternostro@gmail.com>
2021-09-24 20:19:14 -0300

    feat: config env variables

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,6 +1,7 @@
 import { join } from 'path';
 import { Module } from '@nestjs/common';
 import { GraphQLModule } from '@nestjs/graphql';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
@@ -8,6 +9,7 @@ import { TodoModule } from './todo/todo.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot({
       typePaths: ['./**/*.graphql'],
       definitions: {
