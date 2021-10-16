c5ed886da75c2a849add97d036ddd899bdbc44da
Marcelo <marcelodpaternostro@gmail.com>
2021-10-16 12:47:20 -0300

    feat: trim all inputs but password

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,11 @@ import * as cookieParser from 'cookie-parser';
 import { NestFactory } from '@nestjs/core';
 import { ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
+import { TrimPipe } from './trim.pipe';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(new TrimPipe(), new ValidationPipe());
   app.use(cookieParser());
   await app.listen(3000);
 }
